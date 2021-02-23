9b69cc178ddc37abe84af7c823e7d008079595a0
Siddhant <siddhant@Siddhants-MacBook-Pro.local>
2021-02-23 12:58:21 +0530

    Version-4 Search app

--- src/main/java/com/example/springDemo/constants/SolrFieldNames.java
+++ src/main/java/com/example/springDemo/constants/SolrFieldNames.java
@@ -1,8 +1,9 @@
 package com.example.springDemo.constants;
 
-public interface SolarFieldNames {
+public interface SolrFieldNames {
     String NAME = "name";
     String IN_STOCK = "isInStock";
     String DESCRIPTION = "description";
     String OFFER_PRICE = "offerPrice";
+    String STOCK_LOCATION = "stockLocation";
 }
--- src/main/java/com/example/springDemo/service/impl/SearchServiceImpl.java
+++ src/main/java/com/example/springDemo/service/impl/SearchServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.springDemo.service.impl;
 
 import com.example.springDemo.client.SearchClient;
-import com.example.springDemo.constants.SolarFieldNames;
+import com.example.springDemo.constants.SolrFieldNames;
 import com.example.springDemo.dto.ProductResponseDTO;
 import com.example.springDemo.dto.SearchRequestDTO;
 import com.example.springDemo.dto.SearchResponseDTO;
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -17,20 +20,37 @@ public class SearchServiceImpl implements SearchService {
     @Autowired
     private SearchClient searchClient;
 
+
+    public void awaitTerminationAfterShutdown(ExecutorService threadPool) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(1, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
     @Override
     public SearchResponseDTO productSearch(SearchRequestDTO request) {
+        SearchResponseDTO res = new SearchResponseDTO();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> {
+            System.out.println(Thread.currentThread().getId());
+            List<ProductResponseDTO> al = getSearchtermBaseproducts(request.getSearchTerm());
+            res.setProducts(al);
+        });
+        executor.execute(() -> {
+            System.out.println(Thread.currentThread().getId());
+            String q = SolrFieldNames.STOCK_LOCATION+":"+request.getLocation();
+            List<ProductResponseDTO> all = getSearchtermBaseproducts(q);
+            res.setProductsByLocation(all);
+        });
 
-        String locationSearch = "stockLocation:" + request.getLocation();
-        Map<String, Object> productsByLocation = searchClient.getProducts(locationSearch);
-
-        ArrayList<ProductResponseDTO> productList = (ArrayList<ProductResponseDTO>) getSearchtermBaseproducts(request.getSearchTerm());
-        ArrayList<ProductResponseDTO> productListByLoaction = (ArrayList<ProductResponseDTO>) getSearchtermBaseproducts(locationSearch);
-
-        SearchResponseDTO response = new SearchResponseDTO();
-        response.setProducts(productList);
-        response.setProductsByLocation(productListByLoaction);
-        return response;
-
+        awaitTerminationAfterShutdown(executor);
+        return res;
     }
 
     public List<ProductResponseDTO> getSearchtermBaseproducts(String query){
@@ -44,13 +64,13 @@ public class SearchServiceImpl implements SearchService {
         List<ProductResponseDTO> productsList = new ArrayList<>();
 
         for(HashMap<String, Object> obj : locationList){
-            if ((int) obj.get(SolarFieldNames.IN_STOCK) > 0) {
+            if ((int) obj.get(SolrFieldNames.IN_STOCK) > 0) {
                 stock = true;
             } else {
                 stock = false;
             }
-            price = Double.parseDouble(obj.get(SolarFieldNames.OFFER_PRICE).toString());
-            productsList.add(new ProductResponseDTO(stock, (int) price, obj.get(SolarFieldNames.DESCRIPTION).toString(), obj.get(SolarFieldNames.NAME).toString()));
+            price = Double.parseDouble(obj.get(SolrFieldNames.OFFER_PRICE).toString());
+            productsList.add(new ProductResponseDTO(stock, (int) price, obj.get(SolrFieldNames.DESCRIPTION).toString(), obj.get(SolrFieldNames.NAME).toString()));
 
         }
         return productsList;
